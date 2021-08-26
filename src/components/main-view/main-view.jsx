fa5dcb62aac2b569292f45d6f9b1c6a4b30cc486
luisbartolome <luisbartolomeoyarzabal@hotmail.es>
2021-08-26 12:26:15 +0200

    created new folders and files .scss to the task

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,57 +1,101 @@
 import React from 'react';
+import axios from 'axios';
+
+import { LoginView } from '../login-view/login-view';
+import { RegistrationView } from '../registration-view/registration-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+
 
 export class MainView extends React.Component {
 
     constructor() {
         super();
-        this.state = {
-          movies: [
-            { _id: 1, Title: 'Inception', Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.', ImagePath: 'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_UX182_CR0,0,182,268_AL_.jpg'},
-            { _id: 2, Title: 'Shawshank', Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', ImagePath: 'https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg'},
-            { _id: 3, Title: 'Gladiator', Description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.', ImagePath: 'https://m.media-amazon.com/images/M/MV5BMDliMmNhNDEtODUyOS00MjNlLTgxODEtN2U3NzIxMGVkZTA1L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg'}
-        ],
-          selectedMovie: null,
-        };
-      }
-    
-      setSelectedMovie(newSelectedMovie) {
+       // Initial state is set to null
+    this.state = {
+      movies: [],
+      selectedMovie: null,
+      user: null,
+      register: null,
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('https://api-myflix.herokuapp.com/movies')
+      .then((response) => {
         this.setState({
-          selectedMovie: newSelectedMovie,
+          movies: response.data,
         });
-      }
-    
-      render() {
-        const { movies, selectedMovie } = this.state;
-    
-        if (movies.length === 0)
-          return <div className="main-view">The list is empty!</div>;
-    
-        return (
-          <div className="main-view">
-            {selectedMovie ? (
-              <MovieView
-                movie={selectedMovie}
-                onBackClick={(newSelectedMovie) => {
-                  this.setSelectedMovie(newSelectedMovie);
-                }}
-              />
-            ) : (
-              movies.map((movie) => (
-                <MovieCard
-                  key={movie._id}
-                  movieData={movie}
-                  onMovieClick={(movie) => {
-                    this.setSelectedMovie(movie);
-                  }}
-                />
-              ))
-            )}
-          </div>
-        );
-      }
-    }
-    
-// so we  get rid of the curly braces when importing MainView in index.jsx 
-export default MainView;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  /*When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` *property to that movie*/
+
+  setSelectedMovie(movie) {
+    this.setState({
+      selectedMovie: movie,
+    });
+  }
+
+  /* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
+
+  onLoggedIn(user) {
+    this.setState({
+      user,
+    });
+  }
+
+  onRegister(register) {
+    this.setState({
+      register,
+    });
+  }
+
+  render() {
+    const { movies, selectedMovie, user, register } = this.state;
+
+    /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
+    if (!user)
+      return <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />;
+
+    if (!register)
+      return (
+        <RegistrationView
+          onRegister={(register) => this.onRegister(register)}
+        />
+      );
+
+    // Before the movies have been loaded
+    if (movies.length === 0) return <div className="main-view" />;
+
+    return (
+      <div className="main-view">
+        {selectedMovie ? (
+          <MovieView
+            movie={selectedMovie}
+            onBackClick={(newSelectedMovie) => {
+              this.setSelectedMovie(newSelectedMovie);
+            }}
+          />
+        ) : (
+          movies.map((movie) => (
+            <MovieCard
+              key={movie._id}
+              movie={movie}
+              onMovieClick={(newSelectedMovie) => {
+                this.setSelectedMovie(newSelectedMovie);
+              }}
+            />
+          ))
+        )}
+      </div>
+    );
+  }
+}
+
+// so we  get rid of the curly braces when importing MainView in index.jsx
+export default MainView; 
