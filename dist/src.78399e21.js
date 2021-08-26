ca6c3678e3526912c69abebb2f5237608f16a087
luisbartolome <luisbartolomeoyarzabal@hotmail.es>
2021-08-26 23:47:10 +0200

    auto-update files

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -33093,7 +33093,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "4169" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "15437" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
