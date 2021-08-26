c31daf8cf69bccb5dcc14e74c2aed36eb93558aa
luisbartolome <luisbartolomeoyarzabal@hotmail.es>
2021-08-26 13:15:19 +0200

    movie-card file is modified and the proptypes were installed

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export class MovieCard extends React.Component {
   render() {
@@ -14,4 +15,16 @@ export class MovieCard extends React.Component {
       </div>
     );
   }
-} 
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    // Description: PropTypes.string.isRequired,
+    // ImagePath: PropTypes.string.isRequired,
+    // Genre: PropTypes.shape({
+    //   Name: PropTypes.string,
+    // }),
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired,
+};
