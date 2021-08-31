f6fbacced92c31ac594716c112012a1dc68d3a1c
luisbartolome <luisbartolomeoyarzabal@hotmail.es>
2021-08-31 18:06:43 +0200

    upload code

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -33121,7 +33121,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "1068" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "31391" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
